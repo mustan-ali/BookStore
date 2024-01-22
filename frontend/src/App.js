b931e22ff9675e923e584c67b3b3d2a1df34e8be
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-01-22 13:05:12 +0500

    add components & configure Tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+
 function App() {
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<ShowBook />} />
+        <Route path="/create" element={<CreateBook />} />
+        <Route path="/edit/:id" element={<EditBook />} />
+        <Route path="/delete/:id" element={<DeleteBook />} />
+      </Routes>
+
     </div>
   );
 }
